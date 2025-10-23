7048522caff5e6f68d0a1f51c45fb0d9890b7708
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-10-23 16:50:46 -0700

    basic scripting VM POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,86 @@
+import {useState} from 'react';
 import './App.css'
-import VariableInput from './components/VariableInput'
+import {exampleProgram, nextInstruction, ScriptingContext, stepProgram} from './engine';
+import {produce} from "immer";
+
+const ProgramViewer = ({program, pc}: {program: typeof exampleProgram, pc: number}) => {
+  return (
+    <pre className="program-viewer">
+      {program.map((instruction, index) => (
+        <div key={index} className={index === pc ? 'bg-red-400 text-black' : ''}>
+          {instruction.op.name} {instruction.args.length > 0 ? JSON.stringify(instruction.args) : ''}
+        </div>
+      ))}
+    </pre>
+  )
+}
+
+// Button looks 3D, press animation and hover effect
+const Button = ({onClick, disabled, size, primary, children}: {onClick: () => void, disabled?: boolean, primary?: boolean, size?: "xl" | "l" | "m" | "s", children: React.ReactNode}) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`
+        cursor-pointer
+        ${primary ? "bg-blue-500" : "bg-gray-500"}
+        text-white
+        font-bold
+        ${size === "xl" ? "px-8 py-4" : size === "l" ? "px-6 py-3" : size === "m" ? "px-4 py-2" : size === "s" ? "px-2 py-1" : "px-4 py-2"}
+        rounded
+        shadow-lg
+        ${`hover:${primary ? "bg-blue-600" : "bg-gray-600"}`}
+        active:translate-y-1
+        active:shadow-md
+        disabled:opacity-50
+        disabled:cursor-not-allowed
+        ${size === "xl" ? "text-2xl" : size === "l" ? "text-xl" : size === "m" ? "text-lg" : size === "s" ? "text-sm" : "text-base"}
+      `}
+    >
+      {children}
+    </button>
+  )
+}
 
 function App() {
 
+  const [ctx, setCtx] = useState(new ScriptingContext());
+  const [enabled, setEnabled] = useState(true);
+
   return (
-    <VariableInput
-      onChange={(value) => {
-        console.log(`Variable name changed to: ${value}`);
-      }}
-    />
+    <div className="p-4 space-y-4">
+      <div className="space-x-4">
+        <Button primary size="xl" onClick={() => {
+          const instruction = nextInstruction(ctx, exampleProgram);
+          if(instruction) {
+            const newCtx = produce(ctx, draft => {
+              stepProgram(draft, instruction);
+            })
+            setCtx(newCtx);
+          } else {
+            setEnabled(false);
+          }
+        }} disabled={!enabled}>
+          Step
+        </Button>
+        <Button size="xl" onClick={() => {
+          setCtx(new ScriptingContext());
+          setEnabled(true);
+        }}>Reset</Button>
+      </div>
+      <div className="flex space-x-4">
+        <div>
+          <h2>Program</h2>
+          <ProgramViewer program={exampleProgram} pc={ctx.pc} />
+        </div>
+        <div>
+          <h2>Context</h2>
+          <pre>
+            {JSON.stringify(ctx, null, 2)}
+          </pre>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/engine.ts
@@ -0,0 +1,121 @@
+import {immerable} from "immer";
+type ScriptingObject = any;
+
+export class ScriptingContext {
+  [immerable] = true;
+  stack: ScriptingObject[] = [];
+  variables: {[key: string]: ScriptingObject} = {};
+  pc: number = 0;
+}
+
+interface OperationFn<Args extends any[] = any[]> {
+  (ctx: ScriptingContext, ...args: Args): true | void;
+}
+
+export interface CompiledOperation<Args extends any[] = any[]> {
+  op: OperationFn<Args>;
+  args: Args;
+};
+
+function emit<Args extends any[]>(op: OperationFn<Args>, ...args: Args): CompiledOperation<Args> {
+  return {op, args};
+}
+
+type CompiledProgram = CompiledOperation[];
+
+function invariant(condition: boolean, message: string) {
+  if (!condition) {
+    throw new Error(message);
+  }
+}
+
+export function stepProgram(ctx: ScriptingContext, instruction: CompiledOperation): true | void {
+  const result = instruction.op(ctx, ...instruction.args);
+  ctx.pc += 1;
+  return result;
+}
+
+export function nextInstruction(ctx: ScriptingContext, program: CompiledProgram) {
+  return program[ctx.pc];
+}
+
+function pushStack(stack: ScriptingObject[], value: ScriptingObject) {
+  stack.push(value);
+}
+function popStack(stack: ScriptingObject[]) {
+  return stack.pop();
+}
+function pushObject(ctx: ScriptingContext, obj: ScriptingObject) {
+  pushStack(ctx.stack, obj);
+}
+function pushFromIndex(ctx: ScriptingContext) {
+  const index = popStack(ctx.stack);
+  const array = popStack(ctx.stack);
+  invariant(Array.isArray(array), 'Current object is not an array');
+  pushStack(ctx.stack, array[index]);
+}
+function setVariable(ctx: ScriptingContext, name: string) {
+  ctx.variables[name] = popStack(ctx.stack);
+}
+function pushFromVariable(ctx: ScriptingContext, name: string) {
+  pushStack(ctx.stack, ctx.variables[name]);
+}
+function greaterThan(ctx: ScriptingContext) {
+  const b = popStack(ctx.stack);
+  const a = popStack(ctx.stack);
+  pushStack(ctx.stack, a > b);
+}
+function jumpIfTrue(ctx: ScriptingContext, deltaPc: number) {
+  if (popStack(ctx.stack)) {
+    ctx.pc += deltaPc;
+  }
+}
+function addAndPush(ctx: ScriptingContext) {
+  const b = popStack(ctx.stack);
+  const a = popStack(ctx.stack);
+  pushStack(ctx.stack, a + b);
+}
+function pushJsProperty(ctx: ScriptingContext, property: string) {
+  const obj = popStack(ctx.stack);
+  invariant(property in obj, `Property ${property} does not exist on object`);
+  pushStack(ctx.stack, obj[property]);
+}
+
+// Example: Find the max element in an array
+export const exampleProgram: CompiledProgram = [
+  // array = [...]
+  emit(pushObject, [3, 1, 4, 1, 5, 9, 2, 6, 5]),
+  emit(setVariable, 'array'),
+  // max = 0
+  emit(pushObject, 0),
+  emit(setVariable, 'max'),
+  // i = 0
+  emit(pushObject, 0),
+  emit(setVariable, 'i'),
+  // Loop start
+  // max > array[i]
+  emit(pushFromVariable, 'max'),
+  emit(pushFromVariable, 'array'),
+  emit(pushFromVariable, 'i'),
+  emit(pushFromIndex),
+  emit(greaterThan),
+  emit(jumpIfTrue, 4),
+  // max = array[i]
+  emit(pushFromVariable, 'array'),
+  emit(pushFromVariable, 'i'),
+  emit(pushFromIndex),
+  emit(setVariable, 'max'),
+  // i = i + 1
+  emit(pushFromVariable, 'i'),
+  emit(pushObject, 1),
+  emit(addAndPush),
+  emit(setVariable, 'i'),
+
+  // Loop condition: array.length > i
+  emit(pushFromVariable, 'array'),
+  emit(pushJsProperty, 'length'),
+  emit(pushFromVariable, 'i'),
+  emit(greaterThan),
+  emit(jumpIfTrue, -19),
+  // End
+]
