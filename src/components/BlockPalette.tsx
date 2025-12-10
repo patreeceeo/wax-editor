00888f6c838215cc5d4602fe16c9565479b125e3
Patrick Canfield <578371+patreeceeo@users.noreply.github.com>
2025-12-10 13:27:07 -0800

    reorganize utils

--- src/components/BlockPalette.tsx
+++ src/components/BlockPalette.tsx
@@ -1,4 +1,4 @@
-import { cn } from "../lib/utils";
+import { cn } from "../utils";
 
 /** A block item that can be displayed in the palette */
 interface BlockItem {
---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+// Re-export commonly used utilities
+export { cn } from "./ui";
+export type { ClassValue } from "clsx";
+
+export {
+  getTypeName,
+  isObjectOrArray,
+  isObject,
+  isArray,
+  isString,
+  isNumber,
+  isBoolean,
+  isJsPrimitive,
+  getObjectId,
+} from "./runtime";
